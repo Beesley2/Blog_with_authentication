4afb416da525879df5a395f0c5a8f3219250eea8
Callum <callumbeesley2@gmail.com>
2022-09-27 21:47:21 +0100

    Added os module & changed variables to env

--- main.py
+++ main.py
@@ -10,9 +10,10 @@ from forms import *
 from flask_gravatar import Gravatar
 from sqlalchemy.exc import IntegrityError
 from functools import wraps
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
